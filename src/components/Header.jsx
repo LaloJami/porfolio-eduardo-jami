945e859c53078b5e6b4627a69671a61a6ce489ac
LaloJami <eduardolalojami@gmail.com>
2021-10-31 18:54:42 -0500

    [add] Home diseño, estilo y assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/header.css'
+import initialState from '../initialState';
 
 const Header = () => {
+  const socialMedia = initialState.data.contact;
   return (
     <header className="Header">
       <div className="Header-logo">
-        <h1>EJ</h1>
+        <h1>BJ</h1>
       </div>
       <nav className="Header-nav">
         <ul>
@@ -33,13 +35,13 @@ const Header = () => {
         </ul>
       </nav>
       <div className="social-media">
-        <a target="_blank" href="https://github.com/LaloJami">
+        <a target="_blank" href={socialMedia.github}>
         <i className="fab fa-github-square" />
         </a>
-        <a target="_blank" href="https://www.linkedin.com/in/bryan-eduardo-jami-jami">
+        <a target="_blank" href={socialMedia.linkedin}>
         <i className="fab fa-linkedin" />
         </a>
-        <a target="_blank" href="https://twitter.com/eduardo_jami">
+        <a target="_blank" href={socialMedia.twitter}>
         <i className="fab fa-twitter-square" />
         </a>
       </div>
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import initialState from "../initialState";
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+  return{
+    state
+  }
+}
+
+export { useInitialState };
