54c768bb6d1302d1bb6b0116332c77ec95d1ea99
LaloJami <eduardolalojami@gmail.com>
2021-10-31 20:03:56 -0500

    [add] About me maquetacion y estilo

--- src/containers/AboutMe.jsx
+++ src/containers/AboutMe.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import initialState from '../initialState';
+import photo from '../assets/images/photo_eduardo.jpg';
+import '../styles/containers/AboutMe.css'
 
 const AboutMe = () => {
+  const data = initialState.data 
   return (
-    <div>
-      About me
+    <div className="About">
+      <div className="About-container">
+        <div className="About-info">
+          <h3>Sobre mi <i class="fas fa-address-card" /></h3>
+          <p>{data.aboutFull}</p>
+          <p>{data.aboutpr2}</p>
+          <h3>Mis habilidades <i class="fas fa-laptop-code" /></h3>
+          <ul>
+            {data.skills.map(skill => (
+              <li>{skill}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="About-picture">
+          <img src={photo} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
