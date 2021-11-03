614957bcadaa7d98abd9a98ed4712576fe557382
LaloJami <eduardolalojami@gmail.com>
2021-11-02 21:05:57 -0500

    [update] informacion proyectos

--- src/initialState.js
+++ src/initialState.js
@@ -5,7 +5,15 @@ export default {
     'aboutFull': 'Hola, mi nombre es Bryan Jami y estudio ingenería en Tecnologías de la Información en la Universidad Yachay Tech. Tengo experiencia en Multiplatform Software, desarrollo web frontend, machine learning e Internet of Things.',
     'aboutpr2': 'Soy una persona autodidacta y me encanta aprender sobre nuevas tecnologías y/o herramientas más populares en el desarrollo de software. Soy una persona curiosa, analítica y me encanta los retos. Me gusta trabajr en equipo y estoy abierto a aprender de los demás.',
     'skills': ['Git/GitHub', 'HTML5', 'CSS3', 'JavaScript', 'Python', 'MySQL', 'PostgreSQL', 'Machine Learning'],
-    'projects': [],
+    'projects': [
+      {
+        'id': 1,
+        'name': 'Portafolio Eduardo Jami',
+        'alt': 'portafolio Eduardo Jami imagen',
+        'skills': ['React', 'Webpack', 'HTML', 'CSS'],
+        'link': 'https://google.com'
+      }
+    ],
     'contact': {
       'email': 'eduardo-jami@hotmail.com',
       'linkedin': 'https://www.linkedin.com/in/bryan-eduardo-jami-jami',
